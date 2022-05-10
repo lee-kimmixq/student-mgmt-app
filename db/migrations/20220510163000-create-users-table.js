3ef1141b4c19653cb132eaca05ed11e3fa5da68b
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-11 03:20:51 +0800

    created migration to create users table

---
+++ db/migrations/20220510163000-create-users-table.js
@@ -0,0 +1,40 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      display_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      account_type: {
+        allowNull: false,
+        type: Sequelize.ENUM('teacher', 'parent', 'child', 'school'),
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('users');
+  },
+};
