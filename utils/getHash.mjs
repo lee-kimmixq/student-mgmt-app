3a1f2454150a49aedd035b86a827cb2774590b27
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-12 05:49:04 +0800

    removed dotenv import in getHash.mjs and changed way of export default

--- utils/getHash.mjs
+++ utils/getHash.mjs
@@ -1,10 +1,9 @@
 import JSSHA from 'jssha';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-export default (input) => {
+const getHash = (input) => {
   const shaObj = new JSSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
   shaObj.update(`${input}-${process.env.SALT}`);
   return shaObj.getHash('HEX');
 };
+
+export default getHash;
