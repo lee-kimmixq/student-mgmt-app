014b802dc4f4d7f89caeca21b890bad8dc321a59
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-12 10:24:39 +0800

    created LogoutButton component

---
+++ src/components/LogoutButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+
+export default function LogoutButton({ setIsUserLoggedIn }) {
+  const handleLogout = async () => {
+    await axios.delete('/logout');
+    setIsUserLoggedIn(false);
+  };
+
+  return (
+    <button type="button" onClick={handleLogout}>Log Out</button>
+  );
+}
