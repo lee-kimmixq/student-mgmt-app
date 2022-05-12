3c3a264cd7eae3929204c4484029f0a7ae96713d
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-12 10:24:56 +0800

    added LogoutButton to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import LoginForm from './components/LoginForm.jsx';
+import LogoutButton from './components/LogoutButton.jsx';
 
 export default function App() {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
@@ -8,6 +9,7 @@ export default function App() {
     <div>
       {!isUserLoggedIn && <LoginForm setIsUserLoggedIn={setIsUserLoggedIn} />}
       {isUserLoggedIn && <div>Logged In</div>}
+      {isUserLoggedIn && <LogoutButton setIsUserLoggedIn={setIsUserLoggedIn} />}
     </div>
   );
 }
