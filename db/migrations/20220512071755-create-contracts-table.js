8f5f4bfac77746233f301ae551205f4e45bc82ba
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-13 02:59:04 +0800

    created migration to create contracts table

---
+++ db/migrations/20220512071755-create-contracts-table.js
@@ -0,0 +1,50 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('contracts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      teacher_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      parent_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM('requested', 'accepted', 'rejected'),
+      },
+      student_name: {
+        type: Sequelize.STRING,
+      },
+      lesson_rate: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('contracts');
+  },
+};
