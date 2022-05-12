5d02fc4228cac08f84f4c4a80854de356695f446
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-12 11:24:06 +0800

    added SignupForm to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import LoginForm from './components/LoginForm.jsx';
 import LogoutButton from './components/LogoutButton.jsx';
+import SignupForm from './components/SignupForm.jsx';
 
 export default function App() {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   return (
     <div>
+      {!isUserLoggedIn && <SignupForm setIsUserLoggedIn={setIsUserLoggedIn} />}
       {!isUserLoggedIn && <LoginForm setIsUserLoggedIn={setIsUserLoggedIn} />}
       {isUserLoggedIn && <div>Logged In</div>}
       {isUserLoggedIn && <LogoutButton setIsUserLoggedIn={setIsUserLoggedIn} />}
