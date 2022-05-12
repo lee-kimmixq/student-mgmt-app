6aed45bcb63ccc0c534d411861b68922d03146d8
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-12 08:47:37 +0800

    added LoginForm to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm.jsx';
 
 export default function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
   return (
     <div>
-      This is Con Brio
+      {!isUserLoggedIn && <LoginForm setIsUserLoggedIn={setIsUserLoggedIn} />}
+      {isUserLoggedIn && <div>Logged In</div>}
     </div>
   );
 }
