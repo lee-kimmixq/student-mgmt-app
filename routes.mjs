cd837e30040a1ec30295bd1167f629d115d70a99
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-12 10:24:02 +0800

    created logout route in routes.mjs

--- routes.mjs
+++ routes.mjs
@@ -7,9 +7,14 @@ export default function routes(app) {
   const UserController = initUserController(db);
 
   app.post('/login', UserController.login);
+  app.delete('/logout', (_, res) => {
+    res.clearCookie('userId');
+    res.clearCookie('loggedInHash');
+    res.send({});
+  });
 
   // Root route renders Webpack-generated main.html file
-  app.get('/', (request, response) => {
-    response.sendFile(resolve('dist', 'main.html'));
+  app.get('/', (_, res) => {
+    res.sendFile(resolve('dist', 'main.html'));
   });
 }
