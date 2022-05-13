39b6aab7ef9a40f2d57f6a3a8b4f543911120b9f
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-14 03:25:50 +0800

    created StudentsList components and added route to Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
 import LogoutButton from './LogoutButton.jsx';
+import StudentsList from './StudentsList.jsx';
 
 export default function Dashboard({ setIsUserLoggedIn }) {
   return (
     <div>
       Logged In
       <LogoutButton setIsUserLoggedIn={setIsUserLoggedIn} />
+      <Link to="/students">View List of Students</Link>
+      <Routes>
+        <Route path="students" element={<StudentsList />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/StudentsList.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+export default function StudentsList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const { data } = await axios.get('/students');
+      setStudents(data);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  });
+
+  const studentsList = students.map((student) => (
+    <li key={student.id}>
+      {student.studentName}
+      {' '}
+      | $
+      {student.lessonRate}
+      {' '}
+      |
+      {' '}
+      {student.status}
+      {' '}
+    </li>
+  ));
+
+  return (
+    <div>
+      <ul>
+        {studentsList}
+      </ul>
+    </div>
+  );
+}
