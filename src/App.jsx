e30c0d2c5eaa8dee3cce29b106a5aa3e0335fddb
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-13 15:36:22 +0800

    added useEffect in App.jsx to update state whether user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import LoginMain from './components/LoginMain.jsx';
 import LogoutButton from './components/LogoutButton.jsx';
@@ -6,12 +7,26 @@ import LogoutButton from './components/LogoutButton.jsx';
 export default function App() {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
+  useEffect(async () => {
+    try {
+      const { data } = await axios.get('/checkAuth');
+      console.log(data);
+      setIsUserLoggedIn(data.login);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  });
+
   return (
     <BrowserRouter>
       <div>
         {!isUserLoggedIn && <LoginMain setIsUserLoggedIn={setIsUserLoggedIn} />}
-        {isUserLoggedIn && <div>Logged In</div>}
-        {isUserLoggedIn && <LogoutButton setIsUserLoggedIn={setIsUserLoggedIn} />}
+        {isUserLoggedIn && (
+        <div>
+          Logged In
+          <LogoutButton setIsUserLoggedIn={setIsUserLoggedIn} />
+        </div>
+        )}
       </div>
     </BrowserRouter>
   );
