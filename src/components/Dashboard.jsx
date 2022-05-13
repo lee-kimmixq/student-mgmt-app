e75b371f6c3e530b3d2173224501850ef59ef47e
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-14 07:26:46 +0800

    created LessonForm component to create new lesson logs

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,6 +3,7 @@ import { Link, Routes, Route } from 'react-router-dom';
 import LogoutButton from './LogoutButton.jsx';
 import StudentsList from './StudentsList.jsx';
 import LessonsList from './LessonsList.jsx';
+import LessonForm from './LessonForm.jsx';
 
 export default function Dashboard({ setIsUserLoggedIn }) {
   return (
@@ -11,9 +12,11 @@ export default function Dashboard({ setIsUserLoggedIn }) {
       <LogoutButton setIsUserLoggedIn={setIsUserLoggedIn} />
       <Link to="/students">View List of Students</Link>
       <Link to="/lessons">View List of Lesson Logs</Link>
+      <Link to="/new-lesson">Create Log</Link>
       <Routes>
         <Route path="students" element={<StudentsList />} />
         <Route path="lessons" element={<LessonsList />} />
+        <Route path="new-lesson" element={<LessonForm />} />
       </Routes>
     </div>
   );
---
+++ src/components/LessonForm.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function LessonForm() {
+  const [studentList, setStudentList] = useState([]);
+  const [studentId, setStudentId] = useState('');
+  const [studentMessage, setStudentMessage] = useState('');
+  const [details, setDetails] = useState('');
+  const [detailsMessage, setDetailsMessage] = useState('');
+  const [date, setDate] = useState('');
+  const [dateMessage, setDateMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+  useEffect(async () => {
+    try {
+      const { data } = await axios.get('/students/active');
+      const studentsJsx = data.map((el) => (
+        <option key={el.id} value={el.id}>
+          {el.studentName}
+        </option>
+      ));
+      setStudentList(studentsJsx);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  }, []);
+
+  const handleStudentIdChange = (e) => {
+    setStudentId(e.target.value);
+  };
+
+  const handleDetailsChange = (e) => {
+    setDetails(e.target.value);
+  };
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  // TODO: date validation
+
+  const checkBlank = () => {
+    let hasBlank = false;
+    if (studentId === '') {
+      setStudentMessage('Please choose a student');
+      hasBlank = true;
+    }
+    if (details === '') {
+      setDetailsMessage('Please fill the lesson details');
+      hasBlank = true;
+    }
+    if (date === '') {
+      setDateMessage('Please fill in the date');
+      hasBlank = true;
+    }
+    return hasBlank;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (checkBlank()) return;
+    try {
+      const { data } = await axios.post('/lessons', {
+        studentId, details, date,
+      });
+      if (data.success) {
+        setSuccessMessage('Logged lesson successfully!');
+        setStudentId('');
+        setDate('');
+        setDetails('');
+      }
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="date">Date:</label>
+        <input type="date" id="date" value={date} onChange={handleDateChange} />
+        <span>{dateMessage}</span>
+        <br />
+        <label htmlFor="student-id">Student:</label>
+        <select id="student-id" onChange={handleStudentIdChange} value={studentId}>
+          <option value="">Choose Student</option>
+          {studentList}
+        </select>
+        <span>{studentMessage}</span>
+        <br />
+        <label htmlFor="details">Details:</label>
+        <textarea type="text" id="details" value={details} onChange={handleDetailsChange} />
+        <span>{detailsMessage}</span>
+        <br />
+        <button type="submit" onClick={handleSubmit}>Submit Lesson Log</button>
+      </form>
+      {successMessage}
+    </div>
+  );
+}
