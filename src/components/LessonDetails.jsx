29376ac322cd6d7205b5b577445e71b0810de9d9
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-14 08:49:26 +0800

    refactored edit mode of LessonDetails into LessonEditForm component

--- src/components/LessonDetails.jsx
+++ src/components/LessonDetails.jsx
@@ -1,100 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
-import axios from 'axios';
+
+import LessonEditForm from './LessonEditForm.jsx';
 
 export default function LessonDetails({ lesson }) {
   const [isEditMode, setIsEditMode] = useState(false);
 
-  const [studentList, setStudentList] = useState([]);
-  const [studentId, setStudentId] = useState(lesson.contractId);
-  const [studentMessage, setStudentMessage] = useState('');
-  const [details, setDetails] = useState(lesson.details);
-  const [detailsMessage, setDetailsMessage] = useState('');
-  const [date, setDate] = useState(moment(lesson.lessonDate).format('YYYY-MM-DD'));
-  const [dateMessage, setDateMessage] = useState('');
-
-  useEffect(async () => {
-    try {
-      const { data } = await axios.get('/students/active');
-      const studentsJsx = data.map((el) => (
-        <option key={el.id} value={el.id}>
-          {el.studentName}
-        </option>
-      ));
-      setStudentList(studentsJsx);
-    } catch (err) {
-      console.log(err.response.data);
-    }
-  }, []);
-
-  const handleStudentIdChange = (e) => {
-    setStudentId(e.target.value);
-  };
-
-  const handleDetailsChange = (e) => {
-    setDetails(e.target.value);
-  };
-
-  const handleDateChange = (e) => {
-    setDate(e.target.value);
-  };
-
-  // TODO: date validation
-
-  const checkBlank = () => {
-    let hasBlank = false;
-    if (studentId === '') {
-      setStudentMessage('Please choose a student');
-      hasBlank = true;
-    }
-    if (details === '') {
-      setDetailsMessage('Please fill the lesson details');
-      hasBlank = true;
-    }
-    if (date === '') {
-      setDateMessage('Please fill in the date');
-      hasBlank = true;
-    }
-    return hasBlank;
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (checkBlank()) return;
-    try {
-      const { data } = await axios.put(`/lesson/${lesson.id}`, {
-        studentId, details, date,
-      });
-      if (data.success) {
-        setIsEditMode(false);
-      }
-    } catch (err) {
-      console.log(err.response.data);
-    }
-  };
-
   if (isEditMode) {
     return (
-      <div>
-        <form>
-          <label htmlFor="date">Date:</label>
-          <input type="date" id="date" value={date} onChange={handleDateChange} />
-          <span>{dateMessage}</span>
-          <br />
-          <label htmlFor="student-id">Student:</label>
-          <select id="student-id" onChange={handleStudentIdChange} value={studentId}>
-            <option value="">Choose Student</option>
-            {studentList}
-          </select>
-          <span>{studentMessage}</span>
-          <br />
-          <label htmlFor="details">Details:</label>
-          <textarea type="text" id="details" value={details} onChange={handleDetailsChange} />
-          <span>{detailsMessage}</span>
-          <br />
-          <button type="submit" onClick={handleSubmit}>Edit Lesson</button>
-        </form>
-      </div>
+      <LessonEditForm lesson={lesson} setIsEditMode={setIsEditMode} />
     );
   }
   return (
