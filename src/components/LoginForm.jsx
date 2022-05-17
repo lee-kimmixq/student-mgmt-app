6899f1a6a025737cca2d0fa1384caf97760d0279
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-18 03:26:24 +0800

    added styling and toggle password visibility to LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function LoginForm({ setIsUserLoggedIn }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordType, setPasswordType] = useState('password');
+
+  const togglePasswordType = () => {
+    const newType = passwordType === 'password' ? 'text' : 'password';
+    setPasswordType(newType);
+  };
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -27,15 +34,60 @@ export default function LoginForm({ setIsUserLoggedIn }) {
   };
 
   return (
-    <div>
-      <Link to="/signup">Sign Up</Link>
-      <form>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" onChange={handleUsernameChange} />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" onChange={handlePasswordChange} />
-        <button type="button" onClick={handleLogin}>Log In</button>
-      </form>
+    <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+      <div className="max-w-lg mx-auto">
+        <h1 className="text-2xl font-bold text-center text-indigo-600 sm:text-3xl">Con Brio</h1>
+
+        <p className="max-w-md mx-auto mt-4 text-center text-gray-500">
+          Some introduction text
+        </p>
+
+        <form action="" className="p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl">
+          <p className="text-lg font-medium">Sign in to your account</p>
+
+          <div>
+            <label htmlFor="username" className="text-sm font-medium">Username</label>
+
+            <div className="relative mt-1">
+              <input
+                type="text"
+                id="username"
+                onChange={handleUsernameChange}
+                className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                placeholder="Enter username"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="password" className="text-sm font-medium">Password</label>
+
+            <div className="relative mt-1">
+              <input
+                type={passwordType}
+                id="password"
+                className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                placeholder="Enter password"
+                onChange={handlePasswordChange}
+              />
+
+              <button type="button" className="absolute inset-y-0 inline-flex items-center right-4" onClick={togglePasswordType}>
+                <FontAwesomeIcon icon="far fa-eye" />
+              </button>
+            </div>
+          </div>
+
+          <button type="submit" onClick={handleLogin} className="block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg">
+            Sign in
+          </button>
+
+          <p className="text-sm text-center text-gray-500">
+            No account?
+            {' '}
+            <u><Link to="/signup">Sign up</Link></u>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
