e38b2ce25f292d451076e5ee19e554a323c5b1f8
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-19 03:06:22 +0800

    amended css in LoginForm

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -47,19 +47,18 @@ export default function LoginForm({ setIsUserLoggedIn }) {
 
   return (
     <div>
-      <div className={`${alertVisibility} alert alert-error shadow-lg`}>
+      <div className={`absolute ${alertVisibility} alert alert-error shadow-lg`}>
         <div>
           <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
           <span>Wrong username or password!</span>
         </div>
       </div>
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Con Brio</h1>
-            <p className="py-6">Sign in to your account</p>
           </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
               <div className="form-control">
                 <label htmlFor="username" className="label">
