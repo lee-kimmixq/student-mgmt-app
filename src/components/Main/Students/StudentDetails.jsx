40277dadd3ce58b7bd771aadbd6f1051cf2870b9
Kimmi Lee <lee.kimmixq@gmail.com>
2022-05-31 17:43:39 +0800

    added styling for StudentList, added edit student name functionality

---
+++ src/components/Main/Students/StudentDetails.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { mutate } from 'swr';
+import axios from 'axios';
+import getLoginTokenCookie from '../../../../utils/getLoginTokenCookie.mjs';
+
+export default function StudentDetails({ student }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [studentName, setStudentName] = useState(student.studentName);
+
+  const changeStudentStatus = async (contractId, newStatus) => {
+    try {
+      const response = await axios.put(`/api/student/${contractId}`, { status: newStatus }, { headers: { Authorization: `Bearer ${getLoginTokenCookie(document.cookie)}` } });
+      if (response.data.success) {
+        mutate(['/api/students', { headers: { Authorization: `Bearer ${getLoginTokenCookie(document.cookie)}` } }]);
+      }
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
+  const handleEdit = async (contractId) => {
+    try {
+      const response = await axios.put(`/api/student/${contractId}`, { studentName }, { headers: { Authorization: `Bearer ${getLoginTokenCookie(document.cookie)}` } });
+      if (response.data.success) {
+        mutate(['/api/students', { headers: { Authorization: `Bearer ${getLoginTokenCookie(document.cookie)}` } }]);
+      }
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
+  const handleStudentNameChange = (e) => {
+    setStudentName(e.target.value);
+  };
+
+  const acceptButton = (contractId) => <button type="button" className="btn btn-accent btn-xs" onClick={() => { changeStudentStatus(contractId, 'accepted'); }}>Accept</button>;
+
+  const rejectButton = (contractId) => <button type="button" className="btn btn-secondary btn-xs" onClick={() => { changeStudentStatus(contractId, 'rejected'); }}>Reject</button>;
+
+  const inactiveButton = (contractId) => <button type="button" className="btn btn-primary btn-xs" onClick={() => { changeStudentStatus(contractId, 'inactive'); }}>Inactive</button>;
+
+  const requestButton = (contractId) => <button type="button" className="btn btn-xs" onClick={() => { changeStudentStatus(contractId, 'requested'); }}>Reset</button>;
+
+  const editButton = () => <button type="button" className="btn btn-warning btn-xs" onClick={() => { setIsEditMode(true); }}>Edit</button>;
+
+  const studentRow = (
+    <tr key={student.id} className="hover" onClick={() => setIsExpanded(!isExpanded)}>
+      <td>{student.parentName}</td>
+      <td>
+        {student.studentName}
+        {' '}
+        <span className="badge badge-xs badge-ghost"><em>{student.status}</em></span>
+      </td>
+    </tr>
+  );
+
+  if (isExpanded) {
+    return (
+      <>
+        {studentRow}
+        {isEditMode && (
+        <tr>
+          <div className="flex items-center mx-2">
+            <input type="text" id="name" className="input input-bordered" value={studentName} onChange={handleStudentNameChange} />
+            <button type="submit" className="btn btn-sm btn-warning" onClick={() => handleEdit(student.id)}>Edit Student Name</button>
+          </div>
+        </tr>
+        )}
+        <tr>
+          <td>
+            {editButton(student.id)}
+            {(student.status === 'requested' || student.status === 'inactive') && acceptButton(student.id)}
+            {student.status === 'requested' && rejectButton(student.id)}
+            {student.status === 'accepted' && inactiveButton(student.id)}
+            {student.status === 'rejected' && requestButton(student.id)}
+          </td>
+        </tr>
+      </>
+    );
+  }
+
+  return studentRow;
+}
--- src/components/Main/Students/StudentsList.jsx
+++ src/components/Main/Students/StudentsList.jsx
@@ -1,60 +1,33 @@
 import React from 'react';
-import useSWR, { mutate } from 'swr';
-import axios from 'axios';
+import useSWR from 'swr';
 import fetcher from '../../../../utils/fetcher.mjs';
 import getLoginTokenCookie from '../../../../utils/getLoginTokenCookie.mjs';
+import StudentDetails from './StudentDetails.jsx';
 
 export default function StudentsList() {
-  const { data, error } = useSWR(['/api/students', { headers: { Authorization: `Bearer ${getLoginTokenCookie(document.cookie)}` } }], fetcher);
+  const { data: students, error } = useSWR(['/api/students', { headers: { Authorization: `Bearer ${getLoginTokenCookie(document.cookie)}` } }], fetcher);
 
   if (error) return <div>error</div>;
-  if (!data) return <div>loading</div>;
+  if (!students) return <div>loading</div>;
 
-  const changeStudentStatus = async (contractId, newStatus) => {
-    try {
-      const response = await axios.put(`/api/student/${contractId}`, { status: newStatus }, { headers: { Authorization: `Bearer ${getLoginTokenCookie(document.cookie)}` } });
-      if (response.data.success) {
-        mutate(['/api/students', { headers: { Authorization: `Bearer ${getLoginTokenCookie(document.cookie)}` } }]);
-      }
-    } catch (err) {
-      console.log(err.response.data);
-    }
-  };
+  const studentsList = students.map((student) => (
+    <StudentDetails student={student} />
 
-  const acceptButton = (contractId) => <button type="button" onClick={() => { changeStudentStatus(contractId, 'accepted'); }}>✔️</button>;
-
-  const rejectButton = (contractId) => <button type="button" onClick={() => { changeStudentStatus(contractId, 'rejected'); }}>❌</button>;
-
-  const inactiveButton = (contractId) => <button type="button" onClick={() => { changeStudentStatus(contractId, 'inactive'); }}>🚫</button>;
-
-  const requestButton = (contractId) => <button type="button" onClick={() => { changeStudentStatus(contractId, 'requested'); }}>↩️</button>;
-
-  const studentsList = data.map((student) => (
-    <li key={student.id}>
-      {student.parentName}
-      (
-      {student.studentName}
-      )
-      {' '}
-      | $
-      {student.lessonRate}
-      {' '}
-      |
-      {' '}
-      {student.status}
-      {' '}
-      {(student.status === 'requested' || student.status === 'inactive') && acceptButton(student.id)}
-      {student.status === 'requested' && rejectButton(student.id)}
-      {student.status === 'accepted' && inactiveButton(student.id)}
-      {student.status === 'rejected' && requestButton(student.id)}
-    </li>
   ));
 
   return (
-    <div>
-      <ul>
-        {studentsList}
-      </ul>
+    <div className="overflow-y-auto">
+      <table className="table table-fixed w-[414px] text-sm">
+        <thead>
+          <tr>
+            <th>Parent</th>
+            <th>Student</th>
+          </tr>
+        </thead>
+        <tbody>
+          {studentsList}
+        </tbody>
+      </table>
     </div>
   );
 }
